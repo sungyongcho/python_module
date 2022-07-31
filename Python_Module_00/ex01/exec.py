4f262d19e6b3c22a3c9272f2d9bd8ed10212b4c3
Sungyong Cho <to.sungyongcho@gmail.com>
2022-07-31 20:26:58 +0200

    ex01 done

--- Python_Module_00/ex01/exec.py
+++ Python_Module_00/ex01/exec.py
@@ -5,8 +5,13 @@ args = sys.argv[1:]
 
 list = [i[::-1] for i in args]
 
-list
-print(list.reverse())
+# print(list)
 
-for i in range(len(args)):
-    print("", end='', sep='')
+for i in range(len(args) - 1, -1, -1):
+    for j in range(len(args[i]) - 1, -1, -1):
+        if (args[i][j].isupper()):
+            print(args[i][j].lower(), end='', sep='')
+        else:
+            print(args[i][j].upper(), end='', sep='')
+    if (i != 0):
+        print(end=' ', sep='')
