5391e2ead925e89202abb58b30a6e862d676530e
Sungyong Cho <to.sungyongcho@gmail.com>
2022-08-01 16:45:44 +0200

    ex10 done

---
+++ Python_Module_00/ex10/loading.py
@@ -0,0 +1,38 @@
+import sys
+from tqdm import tqdm
+import time
+from time import sleep
+
+
+# ref: https://stackoverflow.com/questions/68211844/python-change-tqdm-bar-style
+#      https://stackoverflow.com/questions/54362541/how-to-change-tqdms-bar-size
+#      https://stackoverflow.com/questions/57473107/how-to-set-the-r-bar-part-of-tqdm
+
+def ft_progress2(listy):
+    # output = tqdm(listy)
+    return tqdm(listy,
+                bar_format="ETA: {remaining_s:.2f}s " +
+                           "[{desc}: {percentage:.0f}%] " +
+                           "[{bar: 20}] " +
+                           "{n_fmt}/{total_fmt} | " +
+                           "elapsed time: {elapsed_s:.2f}s",
+                ascii=' >=')
+
+
+# 1
+listy = range(1000)
+ret = 0
+for elem in ft_progress2(listy):
+    ret += (elem + 3) % 5
+    sleep(0.01)
+print()
+print(ret)
+
+# 2
+# listy = range(3333)
+# ret = 0
+# for elem in ft_progress2(listy):
+#     ret += elem
+# sleep(0.005)
+# print()
+# print(ret)
