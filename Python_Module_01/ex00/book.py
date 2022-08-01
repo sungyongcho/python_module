d18d641e31bce26c0bb8cdec94eaa74ba87e57db
Sungyong Cho <to.sungyongcho@gmail.com>
2022-08-01 22:45:06 +0200

    module01ex00 in progress

---
+++ Python_Module_01/ex00/book.py
@@ -0,0 +1,21 @@
+import sys
+
+# for not printing traceback
+sys.tracebacklimit = 0
+
+
+class Book:
+
+
+    def get_recipe_by_name(self, name):
+
+    """Prints a recipe with the name \texttt{name} and returns the instance"""
+    # ... Your code here ...
+    def get_recipes_by_types(self, recipe_type):
+
+    """Get all recipe names for a given recipe_type """
+    # ... Your code here ...
+    def add_recipe(self, recipe):
+
+    """Add a recipe to the book and update last_update"""
+    # ... Your code here ...
