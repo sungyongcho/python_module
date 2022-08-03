1b9ede2bcfd3e0085d06197c410e19c98c887be3
Sungyong Cho <to.sungyongcho@gmail.com>
2022-08-03 16:15:11 +0200

    line change

--- Python_Module_01/ex04/eval.py
+++ Python_Module_01/ex04/eval.py
@@ -20,14 +20,18 @@ class Evaluator:
 if __name__ == '__main__':
     words = ["Le", "Lorem", "Ipsum", "est", "simple"]
     coefs = [1.0, 2.0, 1.0, 4.0, 0.5]
+
     print("# Zip")
     print(Evaluator.zip_evaluate(coefs, words))
     print("# Enumerate")
     print(Evaluator.enumerate_evaluate(coefs, words))
     print()
+
     print("--different length--")
+
     words = ["Le", "Lorem", "Ipsum", "n’", "est", "pas", "simple"]
     coefs = [0.0, -1.0, 1.0, -12.0, 0.0, 42.42]
+
     print("# Zip")
     print(Evaluator.zip_evaluate(coefs, words))
     print("# Enumerate")
