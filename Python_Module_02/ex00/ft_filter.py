d6ae23d806744f8f80e2a861d600001aa420b915
Sungyong Cho <to.sungyongcho@gmail.com>
2022-08-03 21:31:58 +0200

    mdoule02 ex00 done

---
+++ Python_Module_02/ex00/ft_filter.py
@@ -0,0 +1,20 @@
+from collections.abc import Iterable
+
+
+def ft_filter(function_to_apply, iterable):
+    """Filter the result of function apply to all elements of the iterable.
+    Args:
+        function_to_apply: a function taking an iterable.
+        iterable: an iterable object (list, tuple, iterator).
+    Return:
+        An iterable.
+        None if the iterable can not be used by the function.
+    """
+    # ... Your code here ...
+    if not (isinstance(iterable, list) or
+            isinstance(iterable, tuple) or
+            iter(iterable) is not None):
+        return None
+    for elem in iterable:
+        if function_to_apply(elem) is True:
+            yield elem
