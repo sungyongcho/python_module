8da7ea3478b807eb218b76a0209cf6a14a769d08
Sungyong Cho <to.sungyongcho@gmail.com>
2022-08-03 08:41:36 +0200

    ex02 done

--- Python_Module_01/ex02/test.py
+++ Python_Module_01/ex02/test.py
@@ -2,23 +2,106 @@ from re import I
 from vector import Vector
 
 # row vector
+print("# row vector")
+print()
 print(Vector([[1., 2.]]).shape)
-
-# column vector
+print(Vector([[1., 2.]]).values)
+print()
+print("#column vector")
+print()
 print(Vector([[1.], [2.], [3.]]).shape)
+print(Vector([[1.], [2.], [3.]]).values)
+print()
 
 
-# dot product
-
-# row
+print("# dot product")
+print("# row")
+print()
 print(Vector([[1., 2.0]]).dot(Vector([[5., 2.0]])))
-
-# column
+print()
+print("#column")
+print()
 print(Vector([[2.0], [3.0]]).dot(Vector([[2.0], [3.0]])))
-
-
+print()
 print(Vector([[3.0]]).dot(Vector([[4.0]])))
-
+print()
+print("# T")
+print()
 print(Vector([[12.0, 34.0]]).T().values)
-
 print(Vector([[56.0], [78.0]]).T().values)
+
+print()
+print("# add, radd")
+print()
+a = Vector([[1., 2., 3.]]) + Vector([[4., 5., 6.]])
+print(a.values)
+aa = Vector([[1.], [2.], [3.]]) + Vector([[4.], [5.], [6.]])
+print(aa.values)
+
+# a = Vector([[1., 2., 3.]]) + 4
+# print(a.values)
+
+b = Vector([[4., 5., 6.]])
+
+c = Vector([[7., 8., 9.]]) + b
+print(c.values)
+
+d = b + Vector([[7., 8., 9.]])
+print(d.values)
+
+# sub, rsub
+
+e = Vector([[1., 2., 3.]]) - Vector([[4., 5., 6.]])
+print(d.values)
+
+ee = Vector([[1.], [2.], [3.]]) - Vector([[4.], [5.], [6.]])
+print(ee.values)
+
+f = Vector([[4., 5., 6.]])
+
+g = Vector([[7., 8., 9.]]) - f
+print(g.values)
+
+h = f - Vector([[7., 8., 9.]])
+print(h.values)
+
+# truediv
+
+i = Vector([[1., 2., 3.]]) / 3
+
+j = Vector([[4.], [5.], [6.]]) / 3
+
+print(i.values)
+print(j.values)
+
+# rtruediv (uncomment to test)
+
+# k =  3 / Vector([[1., 2., 3.]])
+
+# l = 3 / Vector([[4.], [5.], [6.]])
+
+
+# mul, rmul
+
+m = Vector([[1., 2., 3.]]) * 3
+
+n = Vector([[4.], [5.], [6.]]) * 3
+
+print(m.values)
+print(n.values)
+
+
+mm = 3 * Vector([[7., 8., 9.]])
+
+nn = 3 * Vector([[10.], [11.], [12.]])
+
+print(mm.values)
+print(nn.values)
+
+# str, repr
+# must be identical, i.e we expect that print(vector) and vector within python interpretor behave the same, see correspond
+
+v1 = Vector([[0.0], [1.0], [2.0], [3.0]])
+
+print(v1)
+print(repr(v1))
--- Python_Module_01/ex02/vector.py
+++ Python_Module_01/ex02/vector.py
@@ -1,9 +1,14 @@
 
-from typing import Type
+import operator
 
+ops = {"+": operator.add,
+       "-": operator.sub,
+       "*": operator.mul,
+       "/": operator.truediv}
 
 # todo:
 # exeception handling for elements of vector
+
 
 class Vector:
 
@@ -66,3 +71,91 @@ class Vector:
             return self.T_row_to_col()
         else:
             return self.T_col_to_row()
+
+    def __add__(self, other):
+        if not isinstance(other, Vector):
+            raise TypeError("only vector can add to each other")
+        if (self.shape != other.shape):
+            raise ValueError("only vector of same shape is allowed")
+        dimension_check = self.shape.index(max(self.shape))
+        tmp = []
+        if dimension_check == 1:
+            for i in range(0, max(self.shape)):
+                tmp.append(self.values[0][i] + other.values[0][i])
+            return Vector([tmp])
+        else:
+            for i in range(0, max(self.shape)):
+                tmp.append([self.values[i][0] + other.values[i][0]])
+            return Vector(tmp)
+
+    def __radd__(self, other):
+        return other + self
+
+    def __sub__(self, other):
+        if not isinstance(other, Vector):
+            raise TypeError("only vector can add to each other")
+        if (self.shape != other.shape):
+            raise ValueError("only vector of same shape is allowed")
+        dimension_check = self.shape.index(max(self.shape))
+        tmp = []
+        if dimension_check == 1:
+            for i in range(0, max(self.shape)):
+                tmp.append(self.values[0][i] - other.values[0][i])
+            return Vector([tmp])
+        else:
+            for i in range(0, max(self.shape)):
+                tmp.append([self.values[i][0] - other.values[i][0]])
+            return Vector(tmp)
+
+    def __rsub__(self, other):
+        return other - self
+
+    def __row_loop(self, var, operator):
+        tmp = []
+        for i in range(0, max(self.shape)):
+            tmp.append(ops[operator](self.values[0][i], var))
+        return Vector([tmp])
+
+    def __col_loop(self, var, operator):
+        tmp = []
+        for i in range(0, max(self.shape)):
+            tmp.append([ops[operator](self.values[i][0], var)])
+        return Vector(tmp)
+
+    def col_loop(self, other, operator):
+        tmp = []
+        for i in range(0, max(self.shape)):
+            tmp.append([ops[operator](self.values[0][i], other.values[0][i])])
+        return Vector([tmp])
+
+    def __truediv__(self, var):
+        if not any([isinstance(var, t) for t in [float, int, complex]]):
+            raise ValueError("division only accepts scalar. (real number")
+        dimension_check = self.shape.index(max(self.shape))
+        if dimension_check == 1:
+            return self.__row_loop(var, "/")
+        else:
+            return self.__col_loop(var, "/")
+
+    def __rtruediv__(self, var):
+        raise NotImplementedError(
+            "Division of a scalar by a Vector is notdefined here.")
+
+    def __mul__(self, var):
+        if not any([isinstance(var, t) for t in [float, int, complex]]):
+            raise ValueError(
+                "multiplication only accepts scalar. (real number)")
+        dimension_check = self.shape.index(max(self.shape))
+        if dimension_check == 1:
+            return self.__row_loop(var, "*")
+        else:
+            return self.__col_loop(var, "*")
+
+    def __rmul__(self, var):
+        return self * var
+
+    def __str__(self):
+        return str(self.values)
+
+    def __repr__(self):
+        return str(self.values)
