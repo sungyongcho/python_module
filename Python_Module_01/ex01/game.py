5e2553195efab0ca6317f755880e8b4b717a534a
Sungyong Cho <to.sungyongcho@gmail.com>
2022-08-09 23:38:32 +0200

    module01ex00 fix typo, module01ex01 create exception for geting None on the name on GotCharacter class

--- Python_Module_01/ex01/game.py
+++ Python_Module_01/ex01/game.py
@@ -1,21 +1,15 @@
-from tkinter import N
-
-
 class GotCharacter:
 
-    def __init__(self, first_name=None, is_alive=None):
-        if first_name:
-            self.first_name = first_name
-        else:
-            self.first_name = None
-        if is_alive:
-            self.is_alive = is_alive
-        else:
-            self.is_alive = True
+    def __init__(self, first_name, is_alive=True):
+        if first_name is None:
+            raise ValueError("first name invalid")
+        self.first_name
+        self.is_alive = is_alive
 
 
 class Stark(GotCharacter):
     """A class representing the Stark family. Or when bad things happen to good people."""
+
     def __init__(self, first_name=None, is_alive=True):
         super().__init__(first_name=first_name, is_alive=is_alive)
         self.family_name = "Stark"
