05c066618c4156d69377c359ba4fcc70bb8dbae1
Sungyong Cho <to.sungyongcho@gmail.com>
2022-08-09 23:51:50 +0200

    module01ex03 removed unnecessary import

--- Python_Module_01/ex03/generator.py
+++ Python_Module_01/ex03/generator.py
@@ -1,7 +1,3 @@
-# function prototype
-from re import I
-
-
 def generator(text, sep=" ", option=None):
     """Splits the text according to sep value and yield the substrings.
     option precise if a action is performed to the substrings before it is yielded.
