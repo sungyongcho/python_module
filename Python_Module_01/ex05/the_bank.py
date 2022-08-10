9d2b109ec8e3612e592727565c96bc210da57bad
Sungyong Cho <to.sungyongcho@gmail.com>
2022-08-11 00:52:57 +0200

    fix

--- Python_Module_01/ex05/the_bank.py
+++ Python_Module_01/ex05/the_bank.py
@@ -4,18 +4,20 @@ class Account(object):
 
     def __init__(self, name, **kwargs):
         self.__dict__.update(kwargs)
+
         self.id = self.ID_COUNT
         Account.ID_COUNT += 1
         self.name = name
         if not hasattr(self, 'value'):
             self.value = 0
+
         if self.value < 0:
             raise AttributeError("Attribute value cannot be negative.")
         if not isinstance(self.name, str):
             raise AttributeError("Attribute name must be a str object.")
 
-        def transfer(self, amount):
-            self.value += amount
+    def transfer(self, amount):
+        self.value += amount
 
 
 class Bank(object):
